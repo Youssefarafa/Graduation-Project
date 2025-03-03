4ef1a52cafc03fc8050598cffc2936e20d25402f
Youssef Arafa <rfhy300@gmail.com>
2025-03-03 21:49:21 +0200

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,39 @@ module.exports = {
   content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js",],
   theme: {
     extend: {
-      fontSize:{
-        'xxs':'0.5rem'
+      animation: {
+        'shake': 'shake 0.3s ease-in-out',
+      },
+      keyframes: {
+        shake: {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '25%': { transform: 'translateX(-5px)' },
+          '50%': { transform: 'translateX(5px)' },
+          '75%': { transform: 'translateX(-5px)' },
+        }
+      },
+      fontSize: {
+        'xxs': '0.5rem'
       },
       fontFamily: {
         'Philosopher': ['Philosopher', 'sans-serif'],
-        'Roboto':['Roboto','sans-serif'],
-        'Quicksand':['Quicksand','sans-serif'],
-        'Poppins':['Poppins','sans-serif'],
+        'Roboto': ['Roboto', 'sans-serif'],
+        'Quicksand': ['Quicksand', 'sans-serif'],
+        'Poppins': ['Poppins', 'sans-serif'],
+        'Signika': ['Signika', 'sans-serif'],
+        'Inter': ['Inter', 'sans-serif'],
       },
       backgroundImage: {
         'lines-notfound': "url('./assets/Images/LinesNotFound.svg')",
       },
-      screens:{
+      screens: {
         'smmd': '700px',
-      }
+        'lgxl': '1150px',
+        'xl2xl': '1300px',
+      },
+      backgroundSize: {
+        '50%': '50%',
+      },
     },
   },
   plugins: [
